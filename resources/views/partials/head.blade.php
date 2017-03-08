97a0ee31273b7a1f39f269745bc896dc0d6b45d1
Stephen Moore <stephenmoore56@msn.com>
2017-03-07 20:56:46 -0600

    Add meta tags for caching

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,10 +11,13 @@
 <meta name="apple-mobile-web-app-capable" content="yes"/>
 <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
 
-{{--cache busting--}}
+<!-- cache busting -->
 <meta http-equiv="cache-control" content="no-cache, must-revalidate, post-check=0, pre-check=0">
-<meta http-equiv="expires" content="0">
-<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
+
 <title>MOORE+DATABASE</title>
 
 <!-- Bootstrap -->
